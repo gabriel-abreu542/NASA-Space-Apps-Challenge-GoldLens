645429d656032e53a85bc8e5bae88fd88a75e936
Pablo Calil <pablocalilsousa@gmail.com>
2025-10-04 17:41:16 -0300

    Alterações cor P_PLANET

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Dashboard from "./ui/sections/Dashboard";
 import BatchAnalysis from "./ui/sections/BatchAnalysis";
 import { Routes, Route } from "react-router-dom";
 import AboutPage from "./ui/sections/AboutPage";
-import IndividualForm from "./ui/primitives/IndividualForm";
+import IndividualForm from "./ui/primitives/IndividualForm";f
 
 /**
  * Componente principal da aplicação
@@ -15,27 +15,26 @@ export default function App() {
   // Hook fictício apenas para simular o clique no botão "Train"
 
   return (
-    <div className="min-h-screen bg-zinc-950 text-zinc-100">
-      {/* Barra de navegação no topo */}
-      <TopNavigation />
+    <div className="flex flex-col min-h-screen bg-zinc-950 text-zinc-100">
+  {/* Barra de navegação no topo */}
+  <TopNavigation />
 
-      <main className="p-6">
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/treinamento" element={<Dashboard />} />
-          <Route path="/analise-individual" element={<IndividualForm />} />
-          <Route path="/analise-em-lote" element={<BatchAnalysis />} />
-          <Route path="/como-funciona" element={<AboutPage />} />
-        </Routes>
-      </main>
+  <main className="flex-grow p-6 px-4 py-6 sm:px-6 lg:px-8">
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/treinamento" element={<Dashboard />} />
+      <Route path="/analise-individual" element={<IndividualForm />} />
+      <Route path="/analise-em-lote" element={<BatchAnalysis />} />
+      <Route path="/como-funciona" element={<AboutPage />} />
+    </Routes>
+  </main>
 
-      <footer className="w-full py-10 text-xs text-zinc-600">
-        <div className="max-w-7xl mx-auto px-6 text-center">
-          © {new Date().getFullYear()} GoldLens. Todos os direitos reservados.
-        </div>
-      </footer>
-
-    
+  <footer className="bg-zinc-950 w-full text-xs text-zinc-600 py-8 sm:py-10">
+    <div className="max-w-7xl mx-auto px-6 text-center">
+      © {new Date().getFullYear()} GoldLens. Todos os direitos reservados.
     </div>
+  </footer>
+</div>
+
   );
 }
